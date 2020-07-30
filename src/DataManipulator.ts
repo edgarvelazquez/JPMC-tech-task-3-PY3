600b66cf9df9b41a5e2de2d416336a535c2f35c3
edgar velazquez <edgar.ra.velazquez@gmail.com>
2020-07-29 23:47:25 -0700

    Create Patch File

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,34 @@
 import { ServerRespond } from './DataStreamer';
 
+//added values to show dessired graph analysis
 export interface Row {
-  stock: string,
-  top_ask_price: number,
-  timestamp: Date,
+    price_abc: number,
+    price_def: number,
+    ratio: number,
+    timestamp: Date,
+    upper_bound: number,
+    lower_bound: number,
+    trigger_alert: number | undefined,
 }
 
-
+//modified values to react to numerical changes specifically the upper bound and lower bound trigger
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]): Row[] {
-    return serverResponds.map((el: any) => {
+  static generateRow(serverRespond: ServerRespond[]): Row {
+      const priceABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) / 2;
+      const priceDEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) / 2;
+      const ratio = priceABC / priceDEF;
+      const upperBound = 1 + 0.01; //this numbers can vary according to user, but 0.02 allows to see a clearer trigger alert on given data
+      const lowerBound = 1 - 0.01;
       return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
+          price_abc: priceABC,
+          price_def: priceDEF,
+          ratio,
+          timestamp: serverRespond[0].timestamp > serverRespond[1].timestamp ?
+              serverRespond[0].timestamp : serverRespond[1].timestamp,
+          upper_bound: upperBound,
+          lower_bound: lowerBound,
+          trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
       };
-    })
+    
   }
 }
